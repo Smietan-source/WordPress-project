e186016b56f0aed5d3456f95272eaa3069a9ec69
Michal <michal.cwiertnia09@gmail.com>
2026-07-08 12:22:12 +0200

    Finishing html site page.php

--- page.php
+++ page.php
@@ -20,11 +20,22 @@
 
             <div class="about-us_content">
                 <h3>About Us</h3>
-                <span>Opis</span>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus soluta debitis deserunt praesentium? 
+                    Iste quae cumque eveniet, repellendus adipisci vel odit minus voluptas praesentium veniam dolorum consectetur delectus accusantium saepe.</span>
             </div>
 
         </section>
 
+        <section>
+
+            <div class="content_place">
+                <h4>Nagłowek na sekcje</h4>
+                <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus architecto perspiciatis ipsum cupiditate
+                     tempore ducimus pariatur non aut modi voluptatibus ut placeat nemo, asperiores saepe vel consequatur! Libero, est hic?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem quia earum ipsum porro tempo
+                     re atque, sed quis culpa nobis inventore possimus voluptates quaerat placeat voluptate minima recusandae numquam, ducimus neque.</span>
+            </div>
+
+        </section>
     </main>
 
 <?php get_footer() ?>
