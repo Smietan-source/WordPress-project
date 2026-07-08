9335c89d3c0a6623558e64b499e6cb20dba8232b
Michal <michal.cwiertnia09@gmail.com>
2026-07-08 10:30:55 +0200

    Adding features to page.php

--- page.php
+++ page.php
@@ -1,19 +1,29 @@
 <?php get_header() ?>
 
+    <main>
 
-<?php
-    while(have_posts()){
-        the_post(); ?>
-        <div class="x">
-            <p>This is a page, not a post</p>
-            <h2>
-                <a href="<?php the_permalink() ?>"><?php the_title() ?></a>    
-            </h2>
-            <div class="content">
-                <?php the_content() ?>
+        <section>
+
+            <div class="ad">
+                <img src="" alt="">
+                <div class="ad_content">
+                    <h2>Przykładowy nagłowek</h2>
+                    <span>Przykładowy tekst</span>
+                    <a href="">Przykładowy link</a>
+                </div>
             </div>
-        </div>
-    <?php }
-?>
+
+        </section>
+
+        <section>
+
+            <div class="about-us_content">
+                <h3>About Us</h3>
+                <span>Opis</span>
+            </div>
+
+        </section>
+
+    </main>
 
 <?php get_footer() ?>
