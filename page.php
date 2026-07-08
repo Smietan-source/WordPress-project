fc4a91df51b36f81f14835a85d38dc40dfb8a6e4
Michal <michal.cwiertnia09@gmail.com>
2026-07-08 11:53:59 +0200

    Adding features: Ad, About us

--- page.php
+++ page.php
@@ -5,10 +5,11 @@
         <section>
 
             <div class="ad">
-                <img src="" alt="">
+                <img src="sunset.png" alt="Przykładowe zdjęcie">
                 <div class="ad_content">
                     <h2>Przykładowy nagłowek</h2>
-                    <span>Przykładowy tekst</span>
+                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                         Consequatur fugit odio temporibus veritatis error. Omnis, error quod? Ab ad nulla ex ipsa esse, aliquam sequi, dicta porro aliquid vel dolor?</span>
                     <a href="">Przykładowy link</a>
                 </div>
             </div>
