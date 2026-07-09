63a8157fd63ef8bba4cc5551c92a32ebdb178e66
Michal <michal.cwiertnia09@gmail.com>
2026-07-09 15:44:18 +0200

    Added some css updates

--- footer.php
+++ footer.php
@@ -1,11 +1,10 @@
     <section>
-
-        <div class="categories">
+        <div class="box-categories">
             <?php 
                     
             $categories = get_the_category();
             foreach($categories as $category){
-                echo "<a href=' " . get_category_link($category->term_id) . "'>" . $category->cat_name . "</a>";
+                echo "<a href='" . get_category_link($category->term_id) . "'>" . $category->cat_name . "</a>";
             }
                     
             ?>
