5c7d3c9ba7ae871d1940051bb9072226bdbd5746
Michal <michal.cwiertnia09@gmail.com>
2026-07-09 14:50:51 +0200

    Added links to categories

--- front-page.php
+++ front-page.php
@@ -44,6 +44,19 @@
                 <p>Brak wpisów do wyświetlenia.</p>
             <?php endif; ?>
         </section>
+
+        <section>
+
+                <?php 
+                
+                $categories = get_the_category();
+                foreach($categories as $category){
+                    echo "<a href=' " . get_category_link($category->term_id) . "'>" . $category->cat_name . "</a>";
+                }
+                
+                ?>
+
+        </section>
     </main>
 
 <?php get_footer() ?>
