e3108547dbcb8598cbd5b948cd210e3a3f1a1155
Michal <michal.cwiertnia09@gmail.com>
2026-07-09 15:21:58 +0200

    Finish category navigation

--- footer.php
+++ footer.php
@@ -1,6 +1,20 @@
-<footer class="footer">
-    <span>Strona wykonana przez Michała @2026</span>
-</footer>
+    <section>
+                
+        <div class="categories">
+            <?php 
+                    
+            $categories = get_the_category();
+            foreach($categories as $category){
+                echo "<a href=' " . get_category_link($category->term_id) . "'>" . $category->cat_name . "</a>";
+            }
+                    
+            ?>
+        </div>
+    </section>
+
+    <footer class="footer">
+        <span>Strona wykonana przez Michała @2026</span>
+    </footer>
 
 <?php wp_footer() ?>
 </body>
--- front-page.php
+++ front-page.php
@@ -45,18 +45,6 @@
             <?php endif; ?>
         </section>
 
-        <section>
-
-                <?php 
-                
-                $categories = get_the_category();
-                foreach($categories as $category){
-                    echo "<a href=' " . get_category_link($category->term_id) . "'>" . $category->cat_name . "</a>";
-                }
-                
-                ?>
-
-        </section>
     </main>
 
 <?php get_footer() ?>
