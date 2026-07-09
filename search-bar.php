da1ed148388e24f70021de8b5c18a4ea08968222
Michal <michal.cwiertnia09@gmail.com>
2026-07-09 15:46:20 +0200

    Added search-bar to github repository

---
+++ search-bar.php
@@ -0,0 +1,14 @@
+<?php
+$search_query = isset($_GET['q']) ? htmlspecialchars($_GET['q'], ENT_QUOTES, 'UTF-8') : '';
+?>
+
+<form action="search-results.php" method="GET" class="search-bar-form">
+    <input 
+        type="text" 
+        name="q" 
+        value="<?php echo $search_query; ?>" 
+        placeholder="Search..." 
+        required
+    >
+    <button type="submit">Search</button>
+</form>
