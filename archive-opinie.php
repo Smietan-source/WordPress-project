92b9134c16e6e429c95524a8e5d8974d9395cf6c
Michal <michal.cwiertnia09@gmail.com>
2026-07-15 15:00:42 +0200

    Added working acf loop in opinions

--- archive-opinie.php
+++ archive-opinie.php
@@ -5,24 +5,46 @@
   <?php if ( have_posts() ) : ?>
     <?php while ( have_posts() ) : the_post(); ?>
       
-      <article class="opinions-archive-posts">
-
-        <div class="opinions-archive-posts-box1">
-            <h2><?php the_title(); ?></h2>
-            <div class="opinions-archive-posts-box2"><?php the_content(); ?></div>
-        </div>
+      <?php if ( have_rows('pictures_to_thing') ) : ?>
+        <?php while ( have_rows('pictures_to_thing') ) : the_row(); ?>
         
-        <?php if ( has_post_thumbnail() ) : ?>
-            <div class="post-archive-opinions-image">
-              <?php the_post_thumbnail('small'); ?>
+          <?php 
+          $num_stars = get_sub_field('num_stars'); 
+          $picture_description = get_sub_field('opinion_description'); 
+          ?>
+
+          <article class="opinions-archive-posts">
+            <div class="opinions-archive-posts-box1">
+                <h2><?php the_title(); ?></h2>
+                
+                <?php if ( $picture_description ) : ?>
+                    <div class="opinions-archive-posts-box2"><?php echo wp_kses_post($picture_description); ?></div>
+                <?php endif; ?>
             </div>
-          <?php endif; ?>
-      </article>
+
+            <?php if ( $num_stars ) : ?>
+                <div class="post-archive-opinions-image">
+                  <?php 
+                  if ( is_array($num_stars) ) {
+                      echo '<img src="' . esc_url($num_stars['sizes']['thumbnail']) . '" alt="' . esc_attr($num_stars['alt']) . '" />';
+                  } 
+                  elseif ( is_numeric($num_stars) ) {
+                      echo wp_get_attachment_image($num_stars, 'thumbnail');
+                  } 
+                  else {
+                      echo '<img src="' . esc_url($num_stars) . '" alt="Opinia obrazek" />';
+                  }
+                  ?>
+                </div>
+            <?php endif; ?>
+          </article>
+
+        <?php endwhile; ?>
+      <?php endif; ?>
 
     <?php endwhile; ?>
-
   <?php else : ?>
-    <p>Brak wpisów w tym archiwum.</p>
+    <p>Brak wpisów.</p>
   <?php endif; ?>
 
 </main>
